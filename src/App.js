9a15f17b53bdf2d37ff0e6946fd7d4ac0b6a1013
Thor Anderson <ta.anderson@gmail.com>
2020-09-30 15:38:14 -0600

    Added simple routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import './App.css'
 import Senators from './Senators'
 import RepresentativeList from './RepresentativeList'
+import Layout from './components/Layout'
+import Welcome from './components/Welcome'
 
 function App() {
   return (
-    <div className="App">
-      <Senators />
-      <RepresentativeList/>
+    <div className='App'>
+      <Layout />
+      <Switch>
+        <Route path='/senate' component={Senators}/>
+        <Route path='/house' component={RepresentativeList} />
+        <Route path='/' exact component={Welcome} />
+      </Switch>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Layout.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import {
+    AppBar,
+    Toolbar,
+    Typography,
+    Button,
+    IconButton,
+    makeStyles,
+} from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
+import { NavLink } from 'react-router-dom'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+    dem: {
+      color: 'blue',
+    },
+    repub: {
+      color: 'red',
+    },
+    navSpacing: {
+      marginRight: '5rem',
+      color: '#fff',
+      textDecoration: 'none'
+    },
+}))
+  
+export default function ButtonAppBar() {
+    const classes = useStyles()
+
+    return (
+        <div className={classes.root}>
+            <AppBar position='static'>
+                <Toolbar>
+                    <IconButton
+                        edge='start'
+                        className={classes.menuButton}
+                        color='inherit'
+                        aria-label='menu'>
+                        <MenuIcon/>
+                    </IconButton>
+                    <NavLink to='/house' className={classes.navSpacing}>House</NavLink>
+                    <NavLink to='/senate' className={classes.navSpacing}>Senate</NavLink>
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
+}
