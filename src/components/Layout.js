5bcb932dd6a71c252bd37f97031c78ec6250a6b0
Thor Anderson <ta.anderson@gmail.com>
2020-10-14 15:15:11 -0600

    LoginDialog closes and opens now.  With padding!

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -90,7 +90,7 @@ export default function ButtonAppBar() {
           </ListItem>
         </List>
       </Drawer>
-      <LoginDialog open={loginOpen}/>
+      <LoginDialog open={loginOpen} onClose={handleDialogToggle}/>
     </div>
   )
 }
--- src/components/LoginDialog.js
+++ src/components/LoginDialog.js
@@ -1,40 +1,51 @@
 import React from 'react'
-import { Dialog, TextField, Button, makeStyles } from '@material-ui/core'
+import { Box, Dialog, TextField, Button, makeStyles } from '@material-ui/core'
 
 const useStyles = makeStyles(() => ({
-    dialogContent: {
-        display: 'flex',
-        flexDirection: 'column',
-        minHeight: 300,
-        minWidth: 400
-    }
+  dialogContent: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: 300,
+    minWidth: 400,
+    padding: 20,
+  },
 }))
 
 const LoginDialog = (props) => {
-    const classes = useStyles()
-    const { open } = props
+  const classes = useStyles()
+  const { open, onClose } = props
 
-    return (
-        <Dialog open={open} aria-labelledby="Login Dialog">
-            <form noValidate autoComplete="off" className={classes.dialogContent}>
-                <h2>Sign in</h2>
-                <TextField
-          id="email-input"
-          label="Email Address"
-          type="email"
-          variant="filled"
-                />
-                <TextField
-          id="password-input"
-          label="Password"
-          type="password"
-          variant="filled"
-                />
-                <Button color='primary'>Cancel</Button>
-                <Button color='primary'>Login</Button>
-            </form>
-        </Dialog>
-    )
+  const handleClose = () => {
+    onClose()
+  }
+
+  return (
+    <Dialog open={open} onClose={handleClose} aria-labelledby='Login Dialog'>
+      <form noValidate autoComplete='off' className={classes.dialogContent}>
+        <h2>Sign in</h2>
+        <TextField
+          id='email-input'
+          label='Email Address'
+          type='email'
+          variant='filled'
+          margin='normal'
+          placeholder='example@gmail.com'
+        />
+        <TextField
+          id='password-input'
+          label='Password'
+          type='password'
+          variant='filled'
+          margin='normal'
+          placeholder='********'
+        />
+        <Box>
+          <Button color='primary' onClick={handleClose}>Cancel</Button>
+          <Button color='primary'>Login</Button>
+        </Box>
+      </form>
+    </Dialog>
+  )
 }
 
 export default LoginDialog
